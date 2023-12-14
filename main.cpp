3cffd73be6e9b114d03725cb9e05d32ff474899e
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-13 23:01:29 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 
 int main() {
-
+    
     //introduction
-    cout << "this automaton accepts a string if the first symbol in alphabet meets even number of times\n\n ";
+    cout << "this automaton accepts a string if the first symbol in char alphabet meets even number of times\n\n ";
 
     // Entering the size of the alphabet
     cout << "Enter the size of the alphabet:  ";
@@ -39,7 +39,7 @@ int main() {
 
     // Entering a string to check for the even inclusion of the first character of the alphabet 
     vector<char> input;
-    cout << "Enter the string to check (length" << stringLength << "): ";
+    cout << "Enter the string to check (length " << stringLength << "): ";
     char symbol;
     for (int i = 0; i < stringLength; ++i) {
         cin >> symbol;
@@ -79,6 +79,82 @@ int main() {
     }
     else {
         cout << "String is not accepted (amount of " << alphabet[0] << " is odd).\n";// Failure- An odd number of the first character of the alphabet in the entered string 
+    }
+   
+
+    // int 
+
+
+    //introduction
+    cout << "this automaton accepts a string if the first symbol in Int alphabet meets even number of times\n\n ";
+
+    // Entering the size of the alphabet
+    cout << "Enter the size of the alphabet:  ";
+    int alphabetSizeI;
+    cin >> alphabetSizeI;
+
+    // The size of the alphabet must be non-negative
+    if (alphabetSizeI <= 0) {
+        cout << "Invalid alphabet size. Exiting...\n";
+        return 1;
+    }
+
+    // User Alphabet Input
+    vector<int> alphabetI;
+    cout << "Enter the alphabet characters (separate with spaces, max " << alphabetSizeI << "): ";
+    for (int i = 0; i < alphabetSizeI; ++i) {
+        char symbolI;
+        cin >> symbolI;
+        alphabetI.push_back(symbolI);
+    }
+
+    //  User Input of String Length
+    cout << "Enter the length of the string: ";
+    int stringLengthI;
+    cin >> stringLengthI;
+
+    // Entering a string to check for the even inclusion of the first character of the alphabet 
+    vector<int> inputI;
+    cout << "Enter the string to check (length" << stringLengthI << "): ";
+    char symbolI;
+    for (int i = 0; i < stringLengthI; ++i) {
+        cin >> symbolI;
+        inputI.push_back(symbolI);
+    }
+
+
+
+    // Creating Transitions for a State Machine
+    map<std::pair<int, int>, int> transitionsI;
+
+
+    //state machine Logic
+    transitionsI[{0, alphabetI[0]}] = 1;//  From even to odd (originally even)
+    transitionsI[{1, alphabetI[0]}] = 0;//  From odd to even
+
+    //  Transitions for all possible dimensions of the alphabet
+    for (int i = 1; i < alphabetSizeI; ++i)
+    {
+        transitionsI[{0, alphabetI[i]}] = 0;
+        transitionsI[{1, alphabetI[i]}] = 1;
+    }
+
+    //Creating a State Machine 
+    FiniteAutomaton<int, int> automatonInt
+    (
+        { 0, 1 },                // States (0-even1-odd number of the first character of the alphabet in a string)
+        alphabetI,              // Alphabet (previously entered by the user)
+        0,                     // Initial state (initially even(=0))
+        0,                     //  Output state (accept a string if the number of inclusions of the first character is even)
+        transitionsI               // Transitions created based on the length of the alphabet 
+    );
+
+    // String Validation 
+    if (automatonInt.checkString(inputI)) {
+        cout << "String is accepted (amount of " << alphabetI[0] << " is even).\n";// successis the even number of the first character of the alphabet in the entered string 
+    }
+    else {
+        cout << "String is not accepted (amount of " << alphabetI[0] << " is odd).\n";// Failure- An odd number of the first character of the alphabet in the entered string 
     }
 
     return 0;
