2cd9afcb81183becd083495c34051d08406ecef4
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-20 22:50:35 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,80 +3,10 @@
 #include <map>
 #include <string>
 
+#include "automaton.hpp"
 
 using namespace std;
 //class for finite automaton 
-template<typename State, typename Alphabet>
-class FiniteAutomaton {
-public:
-
-    //setters for fields 
-  
-    void setStates(const vector<State>& states) {
-        this->states = states;
-    }
-
-    void setAlphabet(const vector<Alphabet>& alphabet) {
-        this->alphabet = alphabet;
-    }
-
-    void setInitial(const State& initial) {
-        this->initial = initial;
-    }
-
-    void setFinal(const State & final) {
-        this->final = final;
-    }
-
-    void setTransitions(const map<pair<State, Alphabet>, State>& transitions) {
-        this->transitions = transitions;
-    }
-
-    //bool method for fsmN1( .1 and .2)
-    bool checkString(const vector<Alphabet>& input) const {
-        State currentState = initial;
-
-        for (const auto& symbol : input) {
-            auto transition = transitions.find({ currentState, symbol });
-
-            if (transition != transitions.end()) {
-                currentState = transition->second;
-            }
-            else {
-                return false;
-            }
-        }
-
-        return currentState == final;
-    }
-
-    // bool method for fsmN2
-    bool checkSubstring(const string& input, const string& substring) const {
-        vector<Alphabet> inputSymbols(input.begin(), input.end());
-
-        // Check if the substring is found
-        for (size_t i = 0; i <= inputSymbols.size() - substring.length(); ++i) {
-            if (checkString(vector<Alphabet>(inputSymbols.begin() + i, inputSymbols.begin() + i + substring.length()))) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-
-private:
-
-    // class fields 
-    vector<State> states;
-    vector<Alphabet> alphabet;
-    State initial;
-    State final;
-    map<pair<State, Alphabet>, State> transitions;
-};
-
-
-
 
 
 
@@ -186,7 +116,7 @@ int main() {
     automatonIntInt.setFinal(0);
     
 
-    //transition
+    //transitiod 
     map<pair<int, int>, int> transitionsIntInt;
     transitionsIntInt[{0, alphabetInt[0]}] = 1;
     transitionsIntInt[{1, alphabetInt[0]}] = 0;
